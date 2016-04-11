c0780f8131bb257ec24472b5cec6e781015402c6
Alexander Hovy <scriptngcode@gmail.com>
2016-04-11 16:15:50 +0200

    Update ArticleScript.js

--- Source/ArticleScript.js
+++ Source/ArticleScript.js
@@ -1,20 +1,15 @@
 //--------------------------------------------------LOADING MAIN
-(function(headerHeight, curScroll, imgPerc, imgPos, imgSize, factor) {
+(function(scale, position, size, factor) {
   if (screen.width < 480) {
-    headerHeight = 150;
-    imgPerc = 170;
+    scale = 170;
   } else if (screen.width >= 480 && screen.width < 720) {
-    headerHeight = 250;
-    imgPerc = 140;
+    scale = 140;
   } else if (screen.width >= 720 && screen.width < 1024) {
-    headerHeight = 250;
-    imgPerc = 120;
+    scale = 120;
   } else {
-    headerHeight = 400;
-    imgPerc = 100;
+    scale = 100;
   }
   
-  curScroll = 0;
   factor = 3;
 
   $(window).scrollTop(0);
@@ -22,14 +17,14 @@
   $(window).scroll(function() {
     //Parallax
     if ($(window).scrollTop() >= 0) {
-      imgPos = ($(window).scrollTop() / factor) * -1;
+      position = ($(window).scrollTop() / factor) * -1;
       $('#parallax').css({
-        'background-position': 'center ' + imgPos + 'px'
+        'background-position': 'center ' + position + 'px'
       });
     } else {
-      imgSize = imgPerc - ($(window).scrollTop() / factor);
+      size = scale - ($(window).scrollTop() / factor);
       $('#parallax').css({
-        'background-size': imgSize + '%'
+        'background-size': size + '%'
       });
     }
     //Hide Image
